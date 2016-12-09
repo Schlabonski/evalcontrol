0158d50000cf0290de0c21226eae5989db94cd7e
Schlabonski <fthielemann@gmx.de>
2016-12-09 15:05:45 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='Evalcontrol',
+        version='0.1',
+        description='Interface for AD Evaluation Board',
+        author='Fabian Thielemann',
+        packages=['evalcontrol']
+      )
