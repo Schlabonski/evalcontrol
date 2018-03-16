0c70c8fb53229e654bdef0a5cd702fe3d8519302
bali control Center <balismartuser@BaliControlCenter.bal>
2018-03-16 16:52:04 +0100

    safely finalize the USB handler

--- evalcontrol/ad9959.py
+++ evalcontrol/ad9959.py
@@ -47,8 +47,8 @@ class AD9959(object):
         else:
             dev = devs[0]
 
+        #dev.reset() # prevent usbErrors
         self.dev = dev
-        dev.set_configuration()
         cnf = dev.configurations()[0]
         intf = cnf[(0,0)]
 
@@ -68,6 +68,9 @@ class AD9959(object):
         # set default value for auto IO update
         self.auto_update = auto_update
         self.set_clock_multiplier(clkmtp)
+
+    def __del__(self):
+        usb.util.dispose_resources(self.dev)
 
     def _reset_usb_handler(self):
         """Resets the usb handler via which communication takes place.
